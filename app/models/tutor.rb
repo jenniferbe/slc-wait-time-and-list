faeadadf745ddd3310ef723c3df1c79eda389715
Salvador Villegas <villegas@berkeley.edu>
2017-11-03 12:13:13 -0700

    [fix] Fixes spelling mistake
    Fixes spelling mistake for a reference to student_queues that may have caused bugs in the future.

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,7 +1,7 @@
 class Tutor < ActiveRecord::Base
   self.primary_key = :sid
   has_many :drop_in_histories
-  has_many :studen_queues
+  has_many :student_queues
 
 
   #moves a student from the queue to the active session.
