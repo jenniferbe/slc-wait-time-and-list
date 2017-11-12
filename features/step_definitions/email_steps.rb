09c9c2685ecf351cf9c0b91a2a3659390103d982
NahRae Seok <wings429@NahRaes-MacBook-Pro.local>
2017-11-12 05:40:15 -0800

    email feature

---
+++ features/step_definitions/email_steps.rb
@@ -0,0 +1,31 @@
+Given(/^There are (\d+) tutors$/) do |arg|
+  pending
+end
+
+And(/^Tutor picked up "([^"]*)"$/) do |arg|
+  pending
+end
+
+Then(/^"([^"]*)" 's waiting position should be (\d+)$/) do |arg|
+  pending
+end
+
+Then(/^"([^"]*)" should receive an email$/) do |arg|
+  pending
+end
+
+Then(/^"([^"]*)" should not receive an email$/) do |arg|
+  pending
+end
+
+And(/^"([^"]*)" checked in$/) do |arg|
+  pending
+end
+
+When(/^"([^"]*)" open the email$/) do |arg|
+  pending
+end
+
+Then(/^"([^"]*)" should see "([^"]*)" in the email subject$/) do |arg1, arg2|
+  pending
+end
