2472d52038285bfd245fc3cb2e50d2d7aa9a5d99
Jennifer B <jbe@berkeley.edu>
2017-11-15 07:24:28 +0000

    fully functional history search

--- app/controllers/history_entries_controller.rb
+++ app/controllers/history_entries_controller.rb
@@ -1,8 +1,17 @@
 class HistoryEntriesController < ApplicationController
   def show
-    @date = params[:date]
-    unless @date.nil?
-      HistoryEntry.where({created_at: @date..(@date + 1.days)})
+    @date = params[:history_dates]
+    if !(@date == nil or @date == "")
+      @date = @date.to_date
+      @history = HistoryEntry.where({created_at: @date..(@date + 1.days)})
+      if @history.count > 0
+        @found = true
+        @drop_in_queue = @history.where({meet_type: "drop-in"})
+        @weekly_queue = @history.where({meet_type: "weekly"})
+        @scheduled_queue = @history.where({meet_type: "scheduled"})
+      else
+        @found = false
+      end
     end
   end
 end
