0dfe328347ec679533ca3daf7fe44bb6401be0ba
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-11-27 00:48:39 -0800

    devise not working anymore

--- app/controllers/tutor_controller.rb
+++
@@ -1,3 +0,0 @@
-class TutorController < ApplicationController
-  before_action :authenticate_tutor!
-end
---
+++ app/controllers/tutors_controller.rb
@@ -0,0 +1,10 @@
+class TutorsController < ApplicationController
+  before_action :authenticate_tutor!
+
+  def index
+    @drop_in_queue = StudentRequest.where(meet_type: "drop-in").where(status: "waiting").order('created_at')
+    @scheduled_queue = StudentRequest.where(meet_type: "scheduled").where(status: "waiting").order('created_at')
+    @weekly_queue = StudentRequest.where(meet_type: "weekly").where(status: "waiting").order('created_at')
+    @active_sessions = StudentRequest.where(status: "active").order('created_at')
+  end
+end
