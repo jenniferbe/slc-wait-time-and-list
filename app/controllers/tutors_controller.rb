1afc0d271cf67e9cb02bda6ea72745b374a88e1c
Maiki Rainton <maikir@berkeley.edu>
2017-11-29 18:15:41 -0800

    move update function to tutor model

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -11,11 +11,44 @@ class TutorsController < ApplicationController
   def activate_session
     student_request = StudentRequest.find(params[:id])
     student_request.update(:status => "active", :tutor_id => current_tutor.id, :start_time => Time.zone.now)
+    send_email_next_in_line
     redirect_to tutors_path
   end
 
   def finish_session
     StudentRequest.find(params[:id]).update(:status => "finished")
     redirect_to tutors_path
+  end
+
+  def send_email_next_in_line
+    #send an email to next person who hasn't been emailed yet
+   
+    @next_student_in_line = StudentRequest.where(:emailed => false)[0] 
+    @studentid = @next_student_in_line.student_id
+    @student = Student.find(@studentid)
+    ExampleMailer.next_in_line_email(@student).deliver_now
+    StudentRequest.where(:student_id => @studentid)[0].update(:emailed => true)
+
+    # if @numStudentsWaiting >= @numActiveTutors
+
+    # @studentList = StudentRequest.where(meet_type: "drop-in").where(status: "waiting").order('created_at')
+
+    # @int = 1
+
+    # @studentList.each do |entry|
+    #   if @int > @numTutor
+    #     break
+    #   end
+    #   if !entry.emailed
+    #     @studentid = entry.student_id
+    #     @student = Student.find(@studentid)
+    #     ExampleMailer.next_in_line_email(@student).deliver_now
+    #     StudentRequest.find(@studentid).update(:emailed => true)
+
+    #   end
+    #   @int += 1
+
+    # end
+
   end
 end
