bb0e1d24cfaab23e1308159731befc3cf339de9c
Alex Yang <alexyang@airbears2-10-142-35-215.airbears2.1918.berkeley.edu>
2017-12-01 00:28:19 -0800

    comment out tutor security redirect

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 require 'byebug'
 
 RSpec.describe Student, type: :model do
-  
+  it "get students"
+
+  it "checks if student is in line based off meet_type"
+
+  it "creates a student_request"
+
 end
