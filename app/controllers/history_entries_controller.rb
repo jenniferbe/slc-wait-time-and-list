85477eb23b7f243a0227fda4648034f631a107a2
Jennifer Be <jbe@berkeley.edu>
2017-12-01 13:16:00 -0800

    Move code out of begin

--- app/controllers/history_entries_controller.rb
+++ app/controllers/history_entries_controller.rb
@@ -2,18 +2,19 @@ class HistoryEntriesController < ApplicationController
   def show
     begin
       @date = params[:history_dates].to_date.in_time_zone
-      @histories = HistoryEntry.get_tables_for_date(@date)
-      if @histories.nil?
-        @header = "No history entries were found for #{@date.strftime('%A, %B %d, %Y')}"
-      else
-        @found = true
-      end
-      @titles = ["Drop In", "Scheduled Appointments", "Weekly Appointments"]
     rescue
       unless params[:history_dates].nil?
         @header = "Please search for a date with the following format: MM-DD-YYYY"
       end
+      return
     end
+    @histories = HistoryEntry.get_tables_for_date(@date)
+    if @histories.nil?
+      @header = "No history entries were found for #{@date.strftime('%A, %B %d, %Y')}"
+    else
+      @found = true
+    end
+    @titles = ["Drop In", "Scheduled Appointments", "Weekly Appointments"]
   end
 
 
