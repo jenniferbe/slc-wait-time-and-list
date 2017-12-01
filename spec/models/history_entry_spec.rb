dfe015af24e2f723e9a0d195f7bb67bec9dd6e68
Jennifer B <jbe@berkeley.edu>
2017-12-01 08:28:51 +0000

    Add model for history entries

--- spec/models/history_entry_spec.rb
+++ spec/models/history_entry_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe HistoryEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  date = "01/01/2017".to_date.in_time_zone
+  describe "#get_tables_for_date" do
+    it 'returns nil if no entries exist' do
+      tables = HistoryEntry.get_tables_for_date(date)
+      expect(tables).to eq(nil)
+    end
+    it 'returns tables if entries exist' do
+      HistoryEntry.create(:student_id => "1234", :course => "English R1A", :sign_in_time => "01/01/2017".to_date.in_time_zone)
+      tables = HistoryEntry.get_tables_for_date(date)
+      history = HistoryEntry.where({sign_in_time: date...(date + 1.days)})
+      drop_in_queue = history.where({meet_type: "drop-in"})
+      weekly_queue = history.where({meet_type: "weekly"})
+      scheduled_queue = history.where({meet_type: "scheduled"})
+      expect(tables).to eq([drop_in_queue, weekly_queue, scheduled_queue])
+    end
+  end
 end
