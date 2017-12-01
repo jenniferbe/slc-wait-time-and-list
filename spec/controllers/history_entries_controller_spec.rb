049e2585ed5d5f1c7478cb3b105b06e4e5f00ab9
Jennifer B <jbe@berkeley.edu>
2017-12-01 20:08:04 +0000

    RSpec tests for history entries controller spec

--- spec/controllers/history_entries_controller_spec.rb
+++ spec/controllers/history_entries_controller_spec.rb
@@ -10,21 +10,29 @@ RSpec.describe HistoryEntriesController, type: :controller do
     end
     describe 'when the date is invaild' do
       it 'does not search date' do
-        expect(HistoryEntry).to_not receive(:get_tables_for_date)
+        expect(HistoryEntry).to_not receive(:get_tables_for_date).and_call_original
         get :show, history_dates: ""
       end
     end
     describe 'when the date exists' do
-      it 'searches dates if it is valid' do
-        expect(HistoryEntry).to receive(:get_tables_for_date)
+      it 'searches dates and returns entries if it is valid' do
+        date = "01/01/2017".to_date.in_time_zone
+        entry = HistoryEntry.create(:student_id => "1234", :course => "English R1A", :sign_in_time => date)
+        entry.save
+        expect(HistoryEntry).to receive(:get_tables_for_date).with(date).and_call_original
+        get :show, history_dates: "01/01/2017"
+      end
+      it 'returns a message if date has no entries' do
+        date = "01/01/2017".to_date.in_time_zone
+        expect(HistoryEntry).to receive(:get_tables_for_date).with(date).and_call_original
         get :show, history_dates: "01/01/2017"
       end
     end
   end
   describe 'get_report' do
     it 'renders an xlsx file' do
-      date = "01/01/2017".in_time_zone.to_date
-      HistoryEntry.create(:student_id => "1234", :course => "English R1A", :sign_in_time => date)
+      date = "01/01/2017".to_date.in_time_zone
+      entry = HistoryEntry.create(:student_id => "1234", :course => "English R1A", :sign_in_time => date)
       get :get_report, id: "01/01/2017", format: :xlsx
     end
   end
