e10b1e9aa0b179200ddf2f936970433ba73dcf0f
Alex Yang <a.yang96@berkeley.edu>
2017-12-02 14:19:48 -0800

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,32 +13,32 @@ Rails.application.routes.draw do
   get 'history_entries' => 'history_entries#show', as: :history_entries
   get 'history_entries/:id' => 'history_entries#get_report', as: :history_report
   
-  resources :student_requests, only: [:index, :destroy]
+  # resources :student_requests, only: [:destroy]
   resources :tutors, only: [:index]
   patch 'tutors/:id/activate_session' => 'tutors#activate_session', as: :tutor_activate_session
   patch 'tutors/:id/finish_session' => 'tutors#finish_session', as: :tutor_finish_session
 
   root 'students#new'
-  get 'students/:id/sign_in' => 'students#sign_in', as: :sign_in_student
+  # get 'students/:id/sign_in' => 'students#sign_in', as: :sign_in_student
   get 'student_requests/:id/wait_time' => 'student_requests#wait_time', as: :wait_time_student_request
   get 'student_requests/:id/confirm' => 'student_requests#confirm', as: :confirm_student_request
-  get 'student_requests/:id/remove' => 'student_requests#remove', as: :remove_student_request
+  # get 'student_requests/:id/remove' => 'student_requests#remove', as: :remove_student_request
 
   #security routes
   post 'app_login' => 'app_security#authenticate', as: :app_authenticate
   get 'app_login' => 'app_security#show', as: :app_firewall
   post 'app_logout' => 'app_security#logout', as: :app_logout
 
-  get 'tutor_login' => 'tutor_security#show', as: :tutor_firewall
-  post 'tutor_login' => 'tutor_security#authenticate', as: :tutor_authenticate
-  post 'tutor_logout' => 'tutor_security#logout', as: :tutor_logout
+  # get 'tutor_login' => 'tutor_security#show', as: :tutor_firewall
+  # post 'tutor_login' => 'tutor_security#authenticate', as: :tutor_authenticate
+  # post 'tutor_logout' => 'tutor_security#logout', as: :tutor_logout
 
   #the following routes exist to allow for redirects to *#create methods since redirec_to only
   #issues redirects to controller actions that have the GET http verb.
-  get 'student_requests/:id/create' => 'student_requests#create', as: :create_student_request
- 
-  patch 'student_requests/:id/activate_session' => 'student_requests#activate_session', as: :activate_session
-  patch 'student_requests/:id/finish_session' => 'student_requests#finish_session', as: :finish_session
+  # get 'student_requests/:id/create' => 'student_requests#create', as: :create_student_request
+  #
+  # patch 'student_requests/:id/activate_session' => 'student_requests#activate_session', as: :activate_session
+  # patch 'student_requests/:id/finish_session' => 'student_requests#finish_session', as: :finish_session
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
