5ea73af0a9a55c99027a059df8f21b1254e21faa
Salvador Villegas <villegas@berkeley.edu>
2017-12-03 04:32:17 +0000

    [fix] Fixes heroku migration failure

--- db/migrate/20171202224703_change_tutor_work_days_types.rb
+++
@@ -1,7 +0,0 @@
-class ChangeTutorWorkDaysTypes < ActiveRecord::Migration
-  def change
-    change_column :tutor_work_days, :start_time, :time
-    change_column :tutor_work_days, :end_time, :time
-    change_column :tutor_work_days, :num_students, :integer
-  end
-end
