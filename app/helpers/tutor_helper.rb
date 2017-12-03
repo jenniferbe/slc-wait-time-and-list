e6211aefcdd5e531cb2f562e59142e084987cd31
Salvador Villegas <villegas@berkeley.edu>
2017-12-03 01:41:02 +0000

    [feat] Adds attribute to identify admins

--- app/helpers/tutor_helper.rb
+++ app/helpers/tutor_helper.rb
@@ -1,2 +1,7 @@
 module TutorHelper
+  def is_admin?
+    if current_user.try(:admin?)
+      # do something
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128235740) do
+ActiveRecord::Schema.define(version: 20171203004928) do
 
   create_table "history_entries", force: :cascade do |t|
     t.integer  "student_id"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20171128235740) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin"
   end
 
   add_index "tutors", ["email"], name: "index_tutors_on_email", unique: true
