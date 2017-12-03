3ca2b3cd5883ea1db6aaaf9b397e778af4191b90
Alex Yang <a.yang96@berkeley.edu>
2017-12-02 16:25:20 -0800

    changed int to double

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128235740) do
+ActiveRecord::Schema.define(version: 20171202232341) do
 
   create_table "history_entries", force: :cascade do |t|
-    t.integer  "student_id"
+    t.integer  "student_id",   limit: 8
     t.string   "course"
-    t.integer  "tutor_sid"
+    t.integer  "tutor_sid",    limit: 8
     t.datetime "start_time"
     t.datetime "end_time"
     t.string   "tutor_notes"
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(version: 20171128235740) do
   end
 
   create_table "student_requests", force: :cascade do |t|
-    t.integer  "student_id"
+    t.integer  "student_id", limit: 8
     t.string   "course"
     t.string   "meet_type"
     t.string   "status"
     t.time     "wait_time"
-    t.integer  "tutor_id"
+    t.integer  "tutor_id",   limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "emailed"
@@ -41,41 +41,42 @@ ActiveRecord::Schema.define(version: 20171128235740) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "sid"
+    t.integer  "sid",                    limit: 8
     t.string   "email"
     t.boolean  "transfer_student"
     t.boolean  "concurrency_student"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.boolean  "domestic_student"
     t.string   "concurrent_institution"
   end
 
   create_table "tutor_work_days", force: :cascade do |t|
-    t.integer  "tutor_id"
-    t.integer  "start_time"
-    t.integer  "end_time"
-    t.string   "num_students"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "tutor_id",     limit: 8
+    t.time     "start_time"
+    t.time     "end_time"
+    t.integer  "num_students"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "tutors", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "sid"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.integer  "sid",                    limit: 8
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "email",                            default: "", null: false
+    t.string   "encrypted_password",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                    default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "active"
   end
 
   add_index "tutors", ["email"], name: "index_tutors_on_email", unique: true
