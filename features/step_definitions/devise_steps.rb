b65f75a892ce770260d56c35a219d3639429e83c
Salvador Villegas <villegas@berkeley.edu>
2017-12-03 21:27:42 +0000

    [test] Fixes broken cucumber tests
    Changes in views for tutor login broke some cucumber tests where since id's were changed.

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -22,8 +22,8 @@ Given /^I am a new, authenticated user$/ do
   Tutor.new(:email => email, :password => password, :password_confirmation => password, :first_name => first_name, :last_name => last_name, :sid => sid).save!
 
   visit '/tutors/sign_in'
-  fill_in "tutor_sid", :with => sid
-  fill_in "tutor_password", :with => password
-  click_button "Log in"
+  fill_in "tutor[sid]", :with => sid
+  fill_in "tutor[password]", :with => password
+  click_button "tutor-submit"
 
 end
