b41e3eb5aa07a8289e1c7fe01cfb3a25e6d106ea
Jennifer B <jbe@berkeley.edu>
2017-12-04 07:10:57 +0000

    [refactor] simplify calculate_wait_time

--- app/models/student_request.rb
+++ app/models/student_request.rb
@@ -22,7 +22,7 @@ class StudentRequest < ActiveRecord::Base
   end
 
   def self.tutor_has_time_to_help?(time_to_leave,time_to_help)
-    if(time_to_leave-time_to_help)/60 > 20
+    if(time_to_leave - time_to_help)/60 > 20
       return true
     end
     return false
@@ -33,7 +33,23 @@ class StudentRequest < ActiveRecord::Base
     time = Time.now.in_time_zone
     active_tutors.each do |tutor|
       start_time = tutor.is_tutoring? ? tutor.get_time_tutor_can_help : time
-      help_queue.push({start_time: start_time, sid: tutor.sid, elt: tutor.expected_leave_time})
+      help_queue.push({start_time: start_time, elt: tutor.expected_leave_time})
+    end
+    help_queue.sort_by {|tutor| tutor[:start_time]}
+  end
+
+  def self.pair_students_and_tutors(help_queue, i)
+    helped, j = false, i % help_queue.length
+    while not helped
+      tutor_time = help_queue[j]
+      if StudentRequest.tutor_has_time_to_help?(tutor_time[:elt].to_time, tutor_time[:start_time])
+        help_queue[j][:start_time] = tutor_time[:start_time] + 30 * 60
+        helped = true
+      else
+        help_queue.delete_at(j)
+        return nil if help_queue.length == 0
+      end
+      j = j % help_queue.length
     end
     help_queue
   end
@@ -43,28 +59,14 @@ class StudentRequest < ActiveRecord::Base
     help_queue = StudentRequest.generate_help_queue
     return nil if help_queue.length == 0
 
-    help_queue = help_queue.sort_by {|tutor| tutor[:start_time]}
     num_in_line = StudentRequest.where(meet_type: "drop-in").where(status: "waiting").count #+1 because that represents the new student
     #but since we atm put the new student in line before confirming there is no +1
 
     #calculate waitimes for student X in line...these items could be saved and cached but it wont be accurate
     #to do so until we know the full schedule of tutors that day(=>tutorworkday) as the system doesn't know when new tutors will come in
     for i in 0...num_in_line
-      j = i%help_queue.length
-      helped = false
-      while(helped == false)
-        tutor_time = help_queue[j]
-        if StudentRequest.tutor_has_time_to_help?(tutor_time["elt".to_sym].to_time,tutor_time["start_time".to_sym])
-          help_queue[j]["start_time".to_sym] = tutor_time["start_time".to_sym] + 30 * 60
-          helped = true
-        else
-          help_queue.delete_at(j)
-          if help_queue.length == 0
-            return nil
-          end
-          j = j%help_queue.length #will set index to next tutor in help_queue
-        end
-      end
+      help_queue = StudentRequest.pair_students_and_tutors(help_queue, i)
+      return nil if help_queue.nil?
     end
     return help_queue[j]["start_time".to_sym] - 30*60
   end
